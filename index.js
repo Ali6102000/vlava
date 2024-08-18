e26611872377f945a249a892954b0cf27ff3228a
Ali Olleik <ali.oleik.2000@gmail.com>
2024-08-18 19:08:50 +0300

    about us done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//fetch services api
 fetch('http://localhost:3000/services')
       .then(response => response.json())
       .then(json => {
@@ -19,6 +20,39 @@ fetch('http://localhost:3000/services')
       }
       
       )
+
+      //fetch aboutus api
+
+fetch('http://localhost:3000/aboutUs')
+      .then(response => response.json())
+      .then(json => {
+        let aboutUs=document.getElementById("aboutUs");
+        json.forEach(element => {
+            let content=document.createElement("div");
+            content.innerHTML=
+            `   <div class="col-lg-3 col-md-6">
+            <div class="stats-item d-flex align-items-center w-100 h-100">
+              <i class="${element.icon}"></i>
+              <div>
+                <span data-purecounter-start="0" data-purecounter-end="${element.end}" data-purecounter-duration="1" class="purecounter"></span>
+                <p>${element.p}</p>
+              </div>
+            </div>
+          </div> `  ;
+
+        
+         
+            aboutUs.appendChild(content);
+            console.log(element.icon);
+                  
+            
+        });
+      }
+      
+      )
+
+
+
 
 
 
